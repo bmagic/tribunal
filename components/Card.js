89e899606b6eb1f18371eded724d62b878712688
bmagic <baptiste@bmagic.fr>
2022-02-24 10:36:11 +0100

    Move code into Card component

---
+++ components/Card.js
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // 
+import { faTwitch, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import DesktopImage from '../components/DesktopImage'
+
+export default function Card({ emission, desktop }) {
+    return <div className="card">
+        <div className="card-image">
+            <a target='_blank' rel="noreferrer" href={`${emission.url}?t=${desktop.time}`}><DesktopImage id={desktop.id} />
+                {desktop.jugement !== "nonjuge" && <img src={`/jugements/${desktop.sanction || desktop.jugement}.png`} className="jugement" />}
+            </a>
+        </div>
+        <div className="card-content">
+            <div className="content ">
+                Audience: {desktop.emission}{desktop.owner && <span>,&nbsp;&nbsp;Accusé·e: {desktop.owner || "Anonyme"}</span>}<br />
+                <span className="is-italic">{emission.date}</span>
+            </div>
+        </div>
+        <footer className="card-footer">
+            <a className="card-footer-item" target='_blank' rel="noreferrer" href={`${emission.url}?t=${desktop.time}`}><FontAwesomeIcon icon={emission.url.search('youtu') !== -1 ? faYoutube : faTwitch} />&nbsp;&nbsp;Voir le jugement</a>
+        </footer>
+    </div>
+}
--- pages/collections.js
+++ pages/collections.js
@@ -1,8 +1,6 @@
 import json from '../public/data.json'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // 
-import { faYoutube } from "@fortawesome/free-brands-svg-icons";
-import DesktopImage from '../components/DesktopImage'
 import Header from '../components/Header';
+import Card from '../components/Card';
 
 export default function Home() {
   return (
@@ -14,22 +12,7 @@ export default function Home() {
 
             const emission = json.emission[desktop.emission - 1]
             return <div key={desktop.id} className="column is-4">
-              <div className="card">
-                <div className="card-image">
-                  <a target='_blank' rel="noreferrer" href={`${emission.url}?t=${desktop.time}`}><DesktopImage id={desktop.id} />
-                    {desktop.jugement !== "nonjuge" && <img src={`/jugements/${desktop.sanction || desktop.jugement}.png`} className="jugement" />}
-                  </a>
-                </div>
-                <div className="card-content">
-                  <div className="content ">
-                    Audience: {desktop.emission}{desktop.owner && <span>,&nbsp;&nbsp;Accusé·e: {desktop.owner || "Anonyme"}</span>}<br />
-                    <span className="is-italic">{emission.date}</span>
-                  </div>
-                </div>
-                <footer className="card-footer">
-                  <a className="card-footer-item" target='_blank' rel="noreferrer" href={`${emission.url}?t=${desktop.time}`}><FontAwesomeIcon icon={faYoutube} />&nbsp;&nbsp;Voir le jugement</a>
-                </footer>
-              </div>
+              <Card desktop={desktop} emission={emission} />
             </div>
 
           })}
