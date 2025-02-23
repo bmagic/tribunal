30e21fab2fe88a95b3bcb7ca1b5c58c4403bf3c6
bmagic <contact@bmagic.fr>
2025-02-23 17:39:19 +0000

    bump version to 1.7.0

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 docker run --privileged --rm tonistiigi/binfmt --install all
-docker --debug buildx build --platform linux/arm64 --tag bmagic/tribunal:1.6.0 --push .
+docker --debug buildx build --platform linux/arm64 --tag bmagic/tribunal:1.7.0 --push .
